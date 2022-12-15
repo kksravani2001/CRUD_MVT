f3be8d8fa44ca8537a588cc108bcac72f9b56ef9
kksravani2001 <kksravani2001@gmail.com>
2022-12-15 10:32:08 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.db.models.functions import Length
 
 # Create your views here.
+from django.db.models import Q
 from app.models import *
 def display_topics(request):
     LTO=Topic.objects.all()
@@ -19,12 +20,30 @@ def display_webpages(request):
     LWO=Webpage.objects.all().order_by('name')
     LWO=Webpage.objects.all().order_by(Length('name'))
     LWO=Webpage.objects.all().order_by(Length('name').desc())
+    LWO=Webpage.objects.all()
+    LWO=Webpage.objects.filter(name__startswith='s')
+    LWO=Webpage.objects.filter(name__endswith='a')
+    LWO=Webpage.objects.filter(name__contains='s')
+    LWO=Webpage.objects.all()
+    LWO=Webpage.objects.filter(name__in=('Meghana','MSD'))
+    LWO=Webpage.objects.filter(name__regex='^M\w{6}')
+    LWO=Webpage.objects.all()
+    LWO=Webpage.objects.filter(Q(topic_name='Cricket') & Q(name__startswith='m'))
+    LWO=Webpage.objects.all()
+    LWO=Webpage.objects.filter(Q(topic_name='Foot Ball') | Q(url__endswith='in'))
+    
+
+
     d={'LWO':LWO}
     return render(request,'display_webpages.html',d) 
      
 
 
 def display_accessrecords(request):
-    LAO=AccessRecords.objects.all()
+    LAO=AccessRecords.objects.filter(date='2001-10-02')  
+    LAO=AccessRecords.objects.filter(date__year='2001')  
+    LAO=AccessRecords.objects.filter(date__year__gte='1800')
+    LAO=AccessRecords.objects.filter(date__year__lte='2001')    
+    
     d={'LAO':LAO}
     return render(request,'display_accessrecords.html',d) 
